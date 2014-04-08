e65f9a4f857ef1ee07f7e6f5c875bd24303178a0
Ariel Churi <git@sparklelabs.com>
2014-04-07 20:38:11 -0400

    touch sensor stays on or off
    removed the timing from the touch sensor.

--- state3.ino
+++ state3.ino
@@ -4,21 +4,22 @@ void state3 () {
         state = 2;
         touchTimer = 100;
         switch (lightState) {
-            case 0:
+            case 0: // The light is off so fade it up.
                 lightState = 2;
                 break;
-            case 1:
+            case 1: // The light is fading up so fade it down.
                 lightState = 0;
                 break;
-            case 2:
+            case 2: // The light is on so fade it out. 
                 lightState = 0;
                 break;
-            case 3:
+            case 3: // The light is fading out so fade it up.
                 lightState = 2;
                 break;
         }
     }
     touchTimer--;
+    /*
     if ( touchTimer == 0 ){ // If the timer has finished change state.
       lightSense();
       if ( dark == true ) { // Switch to either day or night mode.
@@ -29,4 +30,5 @@ void state3 () {
           lightState = 0;
       }
     }
+    */
 }
